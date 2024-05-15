cececdb8849337325952a3ae9be43b7030c66018
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-05-15 16:49:41 -0300

    Actividades de la 1 a la 6 completadas

---
+++ JavaScript/Arrays/Actividad-4/metodo4.js
@@ -0,0 +1,15 @@
+//Ejercicio:
+//Crear una función que reciba un array de palabras y devuelva true si todas las palabras terminan con la letra "a"
+//y false si al menos una palabra no termina con la letra "a".
+
+//Usa el método endsWith() de string para resolverlo.
+
+const array = ["carpa", "tormenta", "casa"];
+const sinA = ["pepe", "carlos", "juan"];
+
+function terminanEnA(words) {
+  const terminaEnA = words.every((word) => word.endsWith("a"));
+  return terminaEnA;
+}
+
+console.log(terminanEnA(array));
---
+++ JavaScript/Arrays/arrays.js
@@ -0,0 +1,14 @@
+const array = [1, "strings", true]; //Como se arma un array
+
+const position = 1; //asignacion valor
+
+console.log(array[position]); //Acceso al valor
+
+array[position] = "Hola"; //Resigno el valor de la posicion
+
+console.log(array[position]);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers[0] = 10;
+numbers[2] = 30;
+console.log(numbers);
