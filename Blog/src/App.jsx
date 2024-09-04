2f906980c60ef64aaed956c72980a51522547d9b
Briancorbalan1801 <bcorbalanbenitez@gmail.com>
2024-09-04 16:41:02 -0300

    Falta cuadros con cosas

---
+++ Blog/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header className="Header">
+        <h1>Blog Sierra de La Ventana</h1>
+      </header>
+
+      <main className="Main">
+        <h3>Bienvenido a mi Blog de Sierra de la ventana</h3>
+      </main>
+
+      <footer className="Footer">
+        <p>&copy; 2024 Mi Portafolio - Todos los derechos reservados</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
