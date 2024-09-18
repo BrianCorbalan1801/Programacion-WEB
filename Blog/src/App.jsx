5536fbb73f1b8f88371abf4bfa3d7c266ff476f6
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-09-18 16:59:34 -0300

    Configurado lo del github para la pagina

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import supabase from "./lib/helper/supabaseClient";
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const {data, error} = await supabase.auth.getSession();
+      if(error) {
+        console.log(error)
+      } else {
+        setUser(data?.session?.user);
+      }
+    };
+  
+    getSession();
+  }, []);
+
+  const handleClick = async () => {
+    const {data, error} = supabase.auth.signInWithOAuth({
+      provider:"github"
+    })
+    if(error) {
+      console.log(error);
+    } else {
+      console.log(data);
+    }
+  }
   return (
-    <div>
-      <header className="Header">
-        <h1>Blog Sierra de La Ventana</h1>
-      </header>
+    <>
+       <div>
+        <header className="Header">
+          <h1>Blog Sierra de La Ventana</h1>
+        <button onClick={handleClick}>Conectar con Github</button>
+        </header>
 
-      <main className="Main">
-        <h3>Bienvenido a mi Blog de Sierra de la ventana</h3>
-      </main>
+        <main className="Main">
+          <h3>Bienvenido a mi Blog de Sierra de la ventana</h3>
+        </main>
 
-      <footer className="Footer">
-        <p>&copy; 2024 Mi Portafolio - Todos los derechos reservados</p>
-      </footer>
-    </div>
+        <footer className="Footer">
+          <p>&copy; 2024 Mi Portafolio - Todos los derechos reservados</p>
+        </footer>
+      </div>
+    </>
   );
-}
-
-export default App;
+};
---
+++ Blog/src/lib/helper/supabaseClient.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+//
+const supabase = createClient(
+    "https://xacbwrsoquymkvddznrg.supabase.co/",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhY2J3cnNvcXV5bWt2ZGR6bnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2NzkyMDQsImV4cCI6MjA0MjI1NTIwNH0.NPT5CtunE5KeAqDs7DXwcRfF4XVUX0vDMpUTpMLFwKo"
+);
+
+export default supabase;
